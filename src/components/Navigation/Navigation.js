5e904c58d2dbebd34f6eec0bc187b56d2632871b
mahmud035 <mahamudulhasan4148@gmail.com>
2022-12-31 15:30:14 +0600

    basic navbar setup

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import './Navigation.css';
 
 const Navigation = () => {
   return (
-    <div>
-      <h1>Navigation</h1>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">TODAY I LEARNED</Navbar.Brand>
+        <Nav className="ms-auto">
+          <Nav.Link href="/">
+            <Button variant="info" className="rounded-pill">
+              SHARE A FACT
+            </Button>
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
 
